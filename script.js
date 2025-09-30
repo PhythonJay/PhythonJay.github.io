f45c6caf5952c6ad512ca82d2cbad9373ca48ed9
coder jay <232235304+PhythonJay@users.noreply.github.com>
2025-10-01 06:50:52 +0800

    ntnlgrfndy

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'DONNAPIKSSSS',//planet layer 1
+  "ILOVEYOUUUSOOMUCHHH",//planet layer 2
+  "I'M ALWAYS HERE FOR YOU",//planet layer 3
+  "YOU'RE MY UNIVERSEEEE",//planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
